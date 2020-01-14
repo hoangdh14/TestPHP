b8d1d5698a622fc9e6bd34b029cbf61bd0aa0dba
hoangdh14 <59603737+hoangdh14@users.noreply.github.com>
2020-01-14 16:24:35 +0700

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 <body>
 <center>
 <?php echo'<p>Hello World </p>'; ?>
-<img src="https://i.pinimg.com/originals/e4/78/57/e47857a588f27c181665ed39454387e7.jpg">
+<img src="https://i.pinimg.com/originals/e4/78/57/e47857a588f27c181665ed39454387e7.jpg"><br>
 <a href="google.com">Google</a>
 <a href="profile.html">Facebook</a>
 </center>
