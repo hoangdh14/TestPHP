5a12676f45c2edefdccae9080ebe18c5488ec948
hoangdh14 <59603737+hoangdh14@users.noreply.github.com>
2020-01-14 16:23:40 +0700

    Update index.php

--- index.php
+++ index.php
@@ -4,9 +4,11 @@
 	<title>PHP Test</title>
 </head>
 <body>
+<center>
 <?php echo'<p>Hello World </p>'; ?>
 <img src="https://i.pinimg.com/originals/e4/78/57/e47857a588f27c181665ed39454387e7.jpg">
 <a href="google.com">Google</a>
 <a href="profile.html">Facebook</a>
+</center>
 </body>
 </html>
