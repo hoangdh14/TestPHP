88ee23e4873aced331df43a39979be64ff0efba8
hoangdh14 <59603737+hoangdh14@users.noreply.github.com>
2020-01-14 16:21:16 +0700

    TestPHP

--- index.php
+++ index.php
@@ -5,5 +5,8 @@
 </head>
 <body>
 <?php echo'<p>Hello World </p>'; ?>
+<img src="https://i.pinimg.com/originals/e4/78/57/e47857a588f27c181665ed39454387e7.jpg">
+<a href="google.com">Google</a>
+<a href="profile.html">Facebook</a>
 </body>
 </html>
